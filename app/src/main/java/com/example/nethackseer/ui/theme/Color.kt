3eca0c5dc00925326ab9b2520aaadec0508c058f
jera1470 <jeremycaceres40@gmail.com>
2025-09-12 15:02:57 -0500

    Theme: added a few more colors and integrated some typography

--- app/src/main/java/com/example/nethackseer/ui/theme/Color.kt
+++ app/src/main/java/com/example/nethackseer/ui/theme/Color.kt
@@ -4,3 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 val DarkRed = Color(0xFF8B0000)
 val Red = Color(0xFFFF0000)
+val LightRed = Color(0xFFFFC0C0)
+val Black = Color(0xFF000000)
+val DarkGray = Color(0xFF444444)
+val White = Color(0xFFFFFFFF)
