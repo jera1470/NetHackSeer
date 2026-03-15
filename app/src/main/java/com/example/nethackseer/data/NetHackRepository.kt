44c11a4d7bdb39261c88cf7809da4483e7e0182f
jera1470 <jeremycaceres40@gmail.com>
2026-03-15 17:05:37 -0500

    Data: new functions NetHackRepository.kt
    mostly to make sure that the repo works, but
    will be used later

--- app/src/main/java/com/example/nethackseer/data/NetHackRepository.kt
+++ app/src/main/java/com/example/nethackseer/data/NetHackRepository.kt
@@ -1,13 +1,14 @@
 package com.example.nethackseer.data
 
+import com.example.nethackseer.data.local.dao.ItemDao
 import com.example.nethackseer.data.local.dao.MonsterDao
+import com.example.nethackseer.data.local.entity.ItemEntity
 import com.example.nethackseer.data.local.entity.MonsterEntity
 import kotlinx.coroutines.flow.Flow
 
-class NetHackRepository(private val monsterDao: MonsterDao) {
-    // Expose all monsters in a stream from local database.
-    // ViewModel collects the Flow.
+class NetHackRepository(private val monsterDao: MonsterDao, private val itemDao: ItemDao) {
     val allMonsters: Flow<List<MonsterEntity>> = monsterDao.getAll()
+    val allItems: Flow<List<ItemEntity>> = itemDao.getAll()
 
     /**
      * Search for monsters by name.
@@ -19,5 +20,15 @@ class NetHackRepository(private val monsterDao: MonsterDao) {
      * Get a monster by name.
      */
     fun getMonsterByName(name: String): Flow<MonsterEntity?> = monsterDao.getMonsterByName(name)
+
+    /**
+     * Search for items by name.
+     */
+    fun searchItems(query: String): Flow<List<ItemEntity>> = itemDao.search(query)
+
+    /**
+     * Get an item by name.
+     */
+    fun getItemByName(name: String): Flow<ItemEntity?> = itemDao.getItemByName(name)
 
 }
